95eac5325c120c6d423cf94d1df49f3d28d8685d
juspreet51 <er.juspreet51@gmail.com>
2021-12-02 02:07:30 +0530

    Updated dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="juspy",
-    version="0.0.2b14",
+    version="0.0.2b15",
     author="Jaspreet Singh",
     author_email="contact@juspreet51.in",
     description="An EDA and modellling assist library",
